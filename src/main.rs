2ba1048cd7685b6f05ee3c1ded8b6a2209c679a4
Harry Rigg <harrison.rigg@student.ntschools.net>
2016-11-22 13:27:38 +0930

    Add basic command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate libc;
+extern crate clap;
 
 use std::io::{ Read, BufRead, Write, stdout, stdin };
 use std::fs::File;
 use std::env;
+
+use clap::{ Arg, App };
 
 struct Interpreter {
     stack: Vec<u8>,
@@ -94,24 +97,58 @@ impl Interpreter {
 }
 
 fn main() {
-    let args: Vec<_> = env::args().collect();
+    let arg_matches = App::new("rs-bf")
+                              .version("0.1.0")
+                              .author("Harrison Rigg <harrison.rigg@student.ntschools.net>")
+                              .about("A simple brainf**k interpreter written in Rust")
+                              .arg(Arg::with_name("stack-size")
+                                .short("s")
+                                .long("stack-size")
+                                .value_name("STACK-SIZE")
+                                .help("Sets the size to be used for the stack/tape")
+                                .takes_value(true)
+                                .required(false))
+                              .arg(Arg::with_name("stack-value")
+                                .short("i")
+                                .long("stack-value")
+                                .value_name("STACK-VALUE")
+                                .help("The value that will fill the stack/tape initialy")
+                                .takes_value(true)
+                                .required(false))
+                              .arg(Arg::with_name("FILE")
+                                .help("The file to interpret")
+                                .required(false)
+                                .index(1))
+                              .get_matches();
 
-    let mut interp = Interpreter::new(30000, 0);
-    
-    if args.len() > 1 {
-        let mut f = match File::open(&args[1]) {
+    let stack_size_str = arg_matches.value_of("stack-size").unwrap_or("30000");
+    let stack_size = match stack_size_str.parse::<usize>() {
+        Ok(ok) => ok,
+        Err(_) => panic!("Please enter a valid number for stack-size")
+    };
+
+    let stack_value_str = arg_matches.value_of("stack-value").unwrap_or("0");
+    let stack_value = match stack_value_str.parse::<u8>() {
+        Ok(ok) => ok,
+        Err(_) => panic!("Please enter a valid number for stack-value")
+    };
+
+    let mut interp = Interpreter::new(stack_size, stack_value);
+
+    if let Some(file_name) = arg_matches.value_of("FILE") {
+        let mut f = match File::open(file_name) {
             Ok(ok) => ok,
-            Err(_) => panic!("Unable to open file: {}", &args[1]),
+            Err(_) => panic!("Unable to open file: {}", file_name)
         };
 
         let mut file_contents = String::new();
         match f.read_to_string(&mut file_contents) {
             Ok(_) => (),
-            Err(_) => panic!("Unable to read from file: {}", &args[1]),
-        }
+            Err(_) => panic!("Failed to read file contents: {}", file_name)
+        };
 
         interp.interp(file_contents);
-    }else{
+    } else {
         let stdin = stdin();
         for line in stdin.lock().lines() {
             interp.interp(line.unwrap());
