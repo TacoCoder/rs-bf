342a407ec25d491fbf5c7b0fd2795900f5524b6d
Harry Rigg <harrison.rigg@student.ntschools.net>
2016-11-22 12:39:54 +0930

    Add more general improvements to code

--- src/main.rs
+++ src/main.rs
@@ -11,76 +11,73 @@ struct Interpreter {
 
 impl Interpreter {
     fn new(stack_size: usize, stack_value: u8) -> Interpreter {
-        let mut interp = Interpreter { stack: Vec::with_capacity(stack_size), stack_ptr: 0 };
-        for i in 0..stack_size {
-            interp.stack.push(stack_value);
-        }
-        interp
+        Interpreter { stack: vec![stack_value; stack_size], stack_ptr: 0 }
     }
 
-    fn interp(&mut self, source: &str) {
-        let char_vec: Vec<char> = source.chars().collect();
+    fn interp(&mut self, source: String) {
+        let mut char_vec: Vec<u8> = Vec::new();
+        (source.as_ref() as &[u8]).read_to_end(&mut char_vec);
 
         let mut i = 0;
         'mainloop: while i < char_vec.len() {
             let c = char_vec[i];
             match c {
-                '>' => {
+                b'>' => {
                     self.stack_ptr = self.stack_ptr.wrapping_add(1);
                 },
-                '<' => {
+                b'<' => {
                     self.stack_ptr = self.stack_ptr.wrapping_sub(1);
                 },
-                '+' => {
+                b'+' => {
                     self.stack[self.stack_ptr] = self.stack[self.stack_ptr].wrapping_add(1);
                 },
-                '-' => {
+                b'-' => {
                     self.stack[self.stack_ptr] = self.stack[self.stack_ptr].wrapping_sub(1);
                 },
-                '.' => {
+                b'.' => {
                     print!("{}", self.stack[self.stack_ptr] as char);
                     stdout().flush();
                 },
-                ',' => {
+                b',' => {
                     let mut ic = 0;
                     unsafe { ic = libc::getchar(); }
                     self.stack[self.stack_ptr] = ic as u8;
                 },
-                '[' => {
+                b'[' => {
                     if self.stack[self.stack_ptr] == 0 {
                         let mut nested = 0;
                         loop { 
                             i+=1;
-                            if char_vec[i] == ']' && nested == 0 {
+                            if char_vec[i] == b']' && nested == 0 {
                                 i+=1;
                                 continue 'mainloop;
                             }
 
-                            if char_vec[i] == ']' && nested != 0 {
+                            if char_vec[i] == b']' && nested != 0 {
                                 nested -= 1;
                             }
 
-                            if char_vec[i] == '[' {
+                            if char_vec[i] == b'[' {
                                 nested += 1; 
                             };
                         }
                     }
                 },
-                ']' => {
+                b']' => {
                     if self.stack[self.stack_ptr] != 0 {
                         let mut nested = 0;
                         loop {
                             i-=1;
-                            if char_vec[i] == '[' && nested == 0 {
+                            if char_vec[i] == b'[' && nested == 0 {
                                 i+=1;
                                 continue 'mainloop;
                             }
 
-                            if char_vec[i] == '[' && nested != 0 {
+                            if char_vec[i] == b'[' && nested != 0 {
                                 nested -= 1;
                             }
 
-                            if char_vec[i] == ']' { 
+                            if char_vec[i] == b']' { 
                                 nested += 1;
                             }
                         }
@@ -96,11 +93,6 @@ impl Interpreter {
     }
 }
 
-
-// Run without args to use stdin
-            // First arg is filename to interpret
-// May or may not run on windows, not sure how libc acts on there, will make it independent of libc soon
-// Next goal is to add a compiler (that will compile to ASM, which can be compiled into an executable)
 fn main() {
     let args: Vec<_> = env::args().collect();
 
@@ -109,20 +101,20 @@ fn main() {
     if args.len() > 1 {
         let mut f = match File::open(&args[1]) {
             Ok(ok) => ok,
-            Err(_) => panic!("Unable to find file: {}", &args[1]),
+            Err(_) => panic!("Unable to open file: {}", &args[1]),
         };
 
         let mut file_contents = String::new();
         match f.read_to_string(&mut file_contents) {
             Ok(_) => (),
-            Err(_) => panic!("Found file but unable to read contents"),
+            Err(_) => panic!("Unable to read from file: {}", &args[1]),
         }
 
-        interp.interp(&file_contents[..]);
+        interp.interp(file_contents);
     }else{
         let stdin = stdin();
         for line in stdin.lock().lines() {
-            interp.interp(&line.unwrap()[..]);
+            interp.interp(line.unwrap());
         }
     }
 }
